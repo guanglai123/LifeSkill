9702e1bd153eb3a6ec537ac0a1dee078f7961cd1
guanglai123 <141440294+guanglai123@users.noreply.github.com>
2024-03-21 00:40:11 +0800

    Delete src/main directory

--- src/main/java/org/guanglai/lifeskill/record/PositionRecord.java
+++
@@ -1,6 +0,0 @@
-package org.guanglai.lifeskill.record;
-
-import org.bukkit.Location;
-
-public record PositionRecord(Location location, String type, String id, Integer amount, Long time, Long coolDown) {
-}
